9b0112a4664a3ff3b577930b7c41eb4282f9e76c
jeffreya <andrew-jeffrey@live.co.uk>
2017-03-25 19:39:37 +0100

    Added lombok, added pojo template for lombok

---
+++ src/main/java/com/codebeast/domain/Campaign.java
@@ -0,0 +1,14 @@
+package com.codebeast.domain;
+
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@Data
+@Builder
+@NoArgsConstructor
+@AllArgsConstructor
+public class Campaign {
+    private long id;
+}
---
+++ src/main/java/com/codebeast/domain/Client.java
@@ -0,0 +1,29 @@
+package com.codebeast.domain;
+
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+import java.util.List;
+
+
+@Data
+@Builder
+@NoArgsConstructor
+@AllArgsConstructor
+public class Client {
+
+    private long id;
+
+    private String name;
+
+    private List<Campaign> campaignList;
+
+//    private List<>
+//
+
+
+
+
+}
