e158f7a44c244254a46e99ae5f8a03536736a2df
jeffreya <andrew-jeffrey@live.co.uk>
2017-03-26 00:49:35 +0100

    Added save spinner Added toast for save / failure

--- src/main/resources/public/js/codebeast/create_client.js
+++ src/main/resources/public/js/codebeast/create_client.js
@@ -1,4 +1,6 @@
 function postForm(){
+    showSave();
+
     var formData = $("#create_client").serializeArray().reduce(function(a, x) { a[ x.name] = x.value; return a; }, {});
     $.ajax({
         type: "POST",
@@ -6,9 +8,25 @@ function postForm(){
         data: JSON.stringify(formData),
         contentType: "application/json",
         success: function(data){
-            alert(data.name);
+            Materialize.toast('Created client ' + data.name, 4000)
+            hideSave();
+            $('#modal1').modal('close');
         },
+        error: function(data){
+            Materialize.toast('Failed to create client', 4000)
+            hideSave();
+        }
     });
+}
+
+function showSave() {
+    $("#save_spinner").addClass("active");
+    $("#create_client_button").addClass('disabled');
+}
+
+function hideSave() {
+    $("#save_spinner").removeClass("active");
+    $("#create_client_button").removeClass('disabled');
 }
 
 $("#create_client_button").click(postForm);
