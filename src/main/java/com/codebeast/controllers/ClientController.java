ee9cf88880ff0fbe89974cf1f28c9ce61e99e412
jeffreya <andrew-jeffrey@live.co.uk>
2017-03-25 21:00:34 +0100

    created post for new client created crudservice for checking adding new items validity

--- src/main/java/com/codebeast/controllers/ClientController.java
+++ src/main/java/com/codebeast/controllers/ClientController.java
@@ -1,10 +1,17 @@
 package com.codebeast.controllers;
 
+import com.codebeast.domain.Client;
+import com.codebeast.service.CRUDService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.ResponseEntity;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.servlet.ModelAndView;
 
+import javax.validation.Valid;
 import java.util.HashMap;
 import java.util.Map;
 
@@ -14,11 +21,25 @@ public class ClientController {
 
     private static final String VIEW_NAME = "clients";
 
+    private final CRUDService crudService;
+
+    @Autowired
+    public ClientController(CRUDService crudService) {
+        this.crudService = crudService;
+    }
 
     @GetMapping
     public ModelAndView getView() {
         final Map<String, String> map = new HashMap<>();
         map.put("pageName", VIEW_NAME);
         return new ModelAndView(VIEW_NAME, map);
+    }
+
+
+    @PostMapping("/create")
+    public Client createClient(@RequestBody @Valid Client client) {
+
+
+        return client;
     }
 }
---
+++ src/main/java/com/codebeast/service/CRUDService.java
@@ -0,0 +1,13 @@
+package com.codebeast.service;
+
+import org.springframework.stereotype.Service;
+
+@Service
+public class CRUDService {
+
+
+
+
+
+
+}
